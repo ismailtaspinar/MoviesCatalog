68217a7ac8713053f2c9caaceb797612b24ad525
ismail taspinar <ismailtaspinar120@gmail.com>
2023-07-10 13:54:16 +0300

    view updates,hiding keys

--- app/src/main/java/com/itapps/moviescatalog/common/Constants.kt
+++ app/src/main/java/com/itapps/moviescatalog/common/Constants.kt
@@ -1,10 +1,13 @@
 package com.itapps.moviescatalog.common
 
+import com.itapps.moviescatalog.BuildConfig
+
+
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
     const val SEARCH = "search/movie"
     const val DETAILS = "movie/{movie_id}"
-    const val API_KEY = "dadfb69121649170351f057cf3b2fcd9"
+    const val API_KEY = BuildConfig.API_KEY
     const val BASE_DETAILS = "https://image.tmdb.org/t/p/w500"
     const val BASE_POSTER = "https://image.tmdb.org/t/p/w154"
 }
